21509059567ef81551b72e8b433ef84ff8cb721a
jsachdeva7 <jsachdeva7@gatech.edu>
2024-02-07 01:47:24 -0500

    Made website (sorry I didn't do earlier commits, will do that next time!)

---
+++ exer3/GoogleSearch/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      fontSize: {
+        mammoth: '6rem',
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
