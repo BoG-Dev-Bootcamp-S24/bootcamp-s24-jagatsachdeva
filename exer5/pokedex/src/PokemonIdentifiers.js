f46bf2ca0ede72538bfd0aa559a1f96dd0c306e5
jsachdeva7 <jsachdeva7@gatech.edu>
2024-02-20 20:51:16 -0500

    Added colors to the pokemon types.

--- exer5/pokedex/src/PokemonIdentifiers.js
+++ exer5/pokedex/src/PokemonIdentifiers.js
@@ -1,9 +1,23 @@
 export const PokemonIdentifiers = ( {dexNumber, data, setDexNumber} ) => {
     
+    const colorOfType = (type) => {
+        let color = null;
+        const types = ["normal", "fire", "water", "electric", "grass", "ice", "fighting", "poison", "ground", "flying", "psychic", "bug", "rock", "ghost", "dragon", "dark", "steel", "fairy"];
+        const hexColors = ["A8A77A", "EE8130", "6390F0", "F7D02C", "7AC74C", "96D9D6", "C22E28", "A33EA1", "E2BF65", "A98FF3", "F95587", "A6B91A", "B6A136", "735797", "6F35FC", "705746", "B7B7CE", "D685AD"];
+        for (let i = 0; i < types.length; i++) {
+            if (type === types[i]) {
+                color = hexColors[i];
+                break;
+            }
+        }
+        return (color);
+    }
+    
     const renderTypes = () => {
         if (data && data.types) {
             return (data.types.map((currentType, index) => (
-                <p key={index} id="singleType">{ currentType.type.name }</p>
+                <p key={index} id="singleType" style={{ backgroundColor: `#${colorOfType(currentType.type.name)}` }}>{ currentType.type.name }</p>
+
             )))
         } else {
             return null;
